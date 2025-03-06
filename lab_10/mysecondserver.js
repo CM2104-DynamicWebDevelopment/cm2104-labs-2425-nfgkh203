d2fdd32166da2d2a17f4ba54a67085482ee17ba8
nfgkh203 <kirilvalchanov@yahoo.com>
2025-03-06 10:31:59 +0000

    new edit

--- lab_10/mysecondserver.js
+++ lab_10/mysecondserver.js
@@ -1,14 +1,4 @@
 var http = require('http');
-
-http.createServer(function (req, res) {
-
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.end('Hello World!');
- 
-}).listen(8080);
-
-
-var http = require('http');
 var currentdate = require('./mymodule');
 http.createServer(function (req, res) {
  res.writeHead(200, {'Content-Type': 'text/html'});
