1b6542101621647d0346152fb5a2988c37cccfa4
nfgkh203 <kirilvalchanov@yahoo.com>
2025-04-16 19:03:17 +0100

    lab5

---
+++ lab_5/errors.js
@@ -0,0 +1,12 @@
+/*function main() {
+    var a, b, c;
+
+	a = 100;
+	b = 10;
+	c = a + (a * b);
+	
+    //document.writeln(c); 
+	console.log(c); //for simple browser check of the output (1100)
+
+}
+*/
